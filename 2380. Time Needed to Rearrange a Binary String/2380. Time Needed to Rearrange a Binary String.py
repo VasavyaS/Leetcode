1630b8fe2506eecaaede47a629b57c550a8bc0d7
Vasavya Sri <vasavyasritm@gmail.com>
2026-01-21 21:58:51 -0800

    Update solution: 2380. Time Needed to Rearrange a Binary String

--- 2380. Time Needed to Rearrange a Binary String/2380. Time Needed to Rearrange a Binary String.py
+++ 2380. Time Needed to Rearrange a Binary String/2380. Time Needed to Rearrange a Binary String.py
@@ -1,18 +1,20 @@
 #
 # Problem: 2380. Time Needed to Rearrange a Binary String
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/time-needed-to-rearrange-a-binary-string/description/?envType=company&envId=salesforce&favoriteSlug=salesforce-three-months
+# Link: https://leetcode.com/problems/time-needed-to-rearrange-a-binary-string/submissions/1892915034/?envType=company&envId=salesforce&favoriteSlug=salesforce-three-months
 # Language: python3
 # Date: 2026-01-22
 
 
 class Solution:
     def secondsToRemoveOccurrences(self, s: str) -> int:
-        count = 0
+        time = 0
+        zero = 0
 
-        while '01' in s:
-            s = s.replace('01', '10')
-            count += 1
-        return count
-
-# O(n^2) O(n)
+        for c in s:
+            if c == '0':
+                zero += 1
+            else:
+                if zero > 0:
+                    time = max(time+1, zero)
+        return time
