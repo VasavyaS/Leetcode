e8fcddd4089a3a28c0ec62646942e541aeb05b48
Vasavya Sri <vasavyasritm@gmail.com>
2026-01-30 20:30:59 -0800

    Add solution: 20. Valid Parentheses

---
+++ 20. Valid Parentheses/20. Valid Parentheses.py
@@ -0,0 +1,22 @@
+#
+# Problem: 20. Valid Parentheses
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/valid-parentheses/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
+# Language: python3
+# Date: 2026-01-31
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hashmap = {')':'(', '}':'{', ']':'['}
+        stack = []
+
+        for i in s:
+            if i in '([{':
+                stack.append(i)
+                continue
+            elif not stack or stack[-1] != hashmap[i]:
+                return False
+            stack.pop()
+        
+        return True if not stack else False            
