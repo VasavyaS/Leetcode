cc802886e19450bd3ec52871c2039cb66f00c62c
Vasavya Sri <vasavyasritm@gmail.com>
2026-01-31 23:06:53 -0800

    Update solution: 125. Valid Palindrome

--- 125. Valid Palindrome/125. Valid Palindrome.py
+++ 125. Valid Palindrome/125. Valid Palindrome.py
@@ -1,14 +1,15 @@
 #
 # Problem: 125. Valid Palindrome
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/valid-palindrome/description/
+# Link: https://leetcode.com/problems/valid-palindrome/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 # Language: python3
-# Date: 2026-01-13
+# Date: 2026-02-01
 
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        i , j = 0, len(s) -1
+        i = 0
+        j = len(s) - 1
 
         while i < j:
             while i < j and not s[i].isalnum():
@@ -19,6 +20,4 @@ class Solution:
                 return False
             i += 1
             j -= 1
-    
         return True
-# O(n) O(1)
