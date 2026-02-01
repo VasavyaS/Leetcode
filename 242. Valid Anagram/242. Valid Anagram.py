cfd0c73836b9efe10164913d8b0e06285ca5d450
Vasavya Sri <vasavyasritm@gmail.com>
2026-02-01 00:41:34 -0800

    Update solution: 242. Valid Anagram

--- 242. Valid Anagram/242. Valid Anagram.py
+++ 242. Valid Anagram/242. Valid Anagram.py
@@ -1,17 +1,15 @@
 #
 # Problem: 242. Valid Anagram
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/valid-anagram/submissions/1881594963/
+# Link: https://leetcode.com/problems/valid-anagram/submissions/1904138257/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 # Language: python3
-# Date: 2026-01-11
+# Date: 2026-02-01
 
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        
         if Counter(s) == Counter(t):
             return True
         return False
-# O(n) O(26)
