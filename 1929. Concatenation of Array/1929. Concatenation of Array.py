c8005bdc9d4fbd24dca50bd8c1f2384631174cec
Vasavya Sri <vasavyasritm@gmail.com>
2026-02-01 22:32:19 -0800

    Add solution: 1929. Concatenation of Array

---
+++ 1929. Concatenation of Array/1929. Concatenation of Array.py
@@ -0,0 +1,12 @@
+#
+# Problem: 1929. Concatenation of Array
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/concatenation-of-array/submissions/1905128328/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
+# Language: python3
+# Date: 2026-02-02
+
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        
+        return nums+nums
