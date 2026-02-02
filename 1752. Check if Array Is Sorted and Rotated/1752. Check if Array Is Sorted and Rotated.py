83aa9ee1fc824cb7d624efefca57165eea304c6f
Vasavya Sri <vasavyasritm@gmail.com>
2026-02-01 22:30:42 -0800

    Update solution: 1752. Check if Array Is Sorted and Rotated

--- 1752. Check if Array Is Sorted and Rotated/1752. Check if Array Is Sorted and Rotated.py
+++ 1752. Check if Array Is Sorted and Rotated/1752. Check if Array Is Sorted and Rotated.py
@@ -1,22 +1,21 @@
 #
 # Problem: 1752. Check if Array Is Sorted and Rotated
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
+# Link: https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/submissions/1905126898/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 # Language: python3
 # Date: 2026-02-02
 
 
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        sorted_num = sorted(nums)
-        
-        for rot in range(len(nums)):
-            flag = True
-            for i in range(len(nums)):
-                if nums[(rot + i)%len(nums)] != sorted_num[i]:
-                    flag = False
-                    break
-            if flag:
-                return True
-        
-        return False
+    # Check the adj elements, %n checks the circular condition. If it exists more than once return False
+        n = len(nums)
+        count = 0
+
+        for i in range(n):
+            if nums[i] > nums[(i + 1)%n]:
+                count += 1
+            if count > 1:
+                return False
+        return True
+# O(n) O(1)
