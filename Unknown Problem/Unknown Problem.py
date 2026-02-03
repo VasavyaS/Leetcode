b656d2234d8595eb41c84cc751b0f2864be2f2b9
Vasavya Sri <vasavyasritm@gmail.com>
2026-02-02 21:36:25 -0800

    Update solution: Unknown Problem

--- Unknown Problem/Unknown Problem.py
+++ Unknown Problem/Unknown Problem.py
@@ -1,28 +1,20 @@
 #
 # Problem: Unknown Problem
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/minimum-pair-removal-to-sort-array-i/submissions/1905159272/
+# Link: https://leetcode.com/problems/reverse-integer/submissions/1906287874/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 # Language: python3
-# Date: 2026-02-02
+# Date: 2026-02-03
 
 
 class Solution:
-    def minimumPairRemoval(self, nums: List[int]) -> int:
-        def isSorted(nums):
-            for i in range(len(nums)-1):
-                if nums[i] > nums[i+1]:
-                    return False
-            return True
-        count = 0
-        while not isSorted(nums):
-            count += 1
-            minSum = float('inf')
-            i1, i2 = 0, 0
-            for i in range(len(nums)-1):
-                if nums[i] + nums[i+1] < minSum:
-                    minSum = nums[i] + nums[i+1]
-                    i1 = i
-                    i2 = i+1
-            nums = nums[:i1] + [minSum] + nums[i2+1:]
+    def reverse(self, x: int) -> int:
+        num = abs(x)
+        rev = 0
+
+        while num != 0:
+            rev = rev * 10 + num % 10
+            num = num // 10
         
-        return count
+        if rev < -2**31 or rev > 2**31 - 1:
+            return 0
+        return rev if x >= 0 else -(rev)
