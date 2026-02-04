54a5dd55c96c872ba3f3578f7aa2acadd19bb2c7
Vasavya Sri <vasavyasritm@gmail.com>
2026-02-03 23:11:17 -0800

    Update solution: Unknown Problem

--- Unknown Problem/Unknown Problem.py
+++ Unknown Problem/Unknown Problem.py
@@ -1,20 +1,21 @@
 #
 # Problem: Unknown Problem
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/reverse-integer/submissions/1906287874/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
+# Link: https://leetcode.com/problems/merge-intervals/submissions/1907635166/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 # Language: python3
-# Date: 2026-02-03
+# Date: 2026-02-04
 
 
 class Solution:
-    def reverse(self, x: int) -> int:
-        num = abs(x)
-        rev = 0
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda x:x[0])
+        res = [intervals[0]]
 
-        while num != 0:
-            rev = rev * 10 + num % 10
-            num = num // 10
-        
-        if rev < -2**31 or rev > 2**31 - 1:
-            return 0
-        return rev if x >= 0 else -(rev)
+        for i, j in intervals:
+            lasti, lastj = res[-1]
+            if i <= lastj:
+                res[-1][1] = max(j, lastj)
+            else:
+                res.append([i, j])
+        return res
+    # O(n log n)
