ae706fa6a3fba9ed1a99108fafbe11152f583018
Vasavya Sri <vasavyasritm@gmail.com>
2026-02-03 22:27:46 -0800

    Update solution: 49. Group Anagrams

--- 49. Group Anagrams/49. Group Anagrams.py
+++ 49. Group Anagrams/49. Group Anagrams.py
@@ -1,20 +1,18 @@
 #
 # Problem: 49. Group Anagrams
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/group-anagrams/description/
+# Link: https://leetcode.com/problems/group-anagrams/submissions/1907591076/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 # Language: python3
-# Date: 2026-01-11
+# Date: 2026-02-04
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = collections.defaultdict(list)
+        res = defaultdict(list)
 
         for s in strs:
-            count = [0] * 26
-            for i in s:
-                count[ord(i) - ord('a')] += 1
+            count = [0]*26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
             res[tuple(count)].append(s)
         return list(res.values())
-
-# O(n*m) O(n*m)
