47d799679411607e2500ef10bf7509974091704a
Vasavya Sri <vasavyasritm@gmail.com>
2026-02-05 00:12:28 -0800

    Update solution: 238. Product of Array Except Self

--- 238. Product of Array Except Self/238. Product of Array Except Self.py
+++ 238. Product of Array Except Self/238. Product of Array Except Self.py
@@ -1,23 +1,22 @@
 #
 # Problem: 238. Product of Array Except Self
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/product-of-array-except-self/description/
+# Link: https://leetcode.com/problems/product-of-array-except-self/submissions/1908871795/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 # Language: python3
-# Date: 2026-01-11
+# Date: 2026-02-05
 
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = []
         prefix = 1
-        
+        res = []
         for i in nums:
             res.append(prefix)
             prefix *= i
         postfix = 1
-        for i in range(len(nums)-1, -1, -1):    
+
+        for i in range(len(nums)-1, -1, -1):
             res[i] *= postfix
             postfix *= nums[i]
         return res
 
-# O(n) O(n)
