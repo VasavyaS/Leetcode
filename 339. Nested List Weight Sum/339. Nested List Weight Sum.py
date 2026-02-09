4ae649e5ce7fbb4299a2b74bd4021101027fd1cb
Vasavya Sri <vasavyasritm@gmail.com>
2026-02-08 18:22:29 -0800

    Add solution: 339. Nested List Weight Sum

---
+++ 339. Nested List Weight Sum/339. Nested List Weight Sum.py
@@ -0,0 +1,68 @@
+#
+# Problem: 339. Nested List Weight Sum
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/nested-list-weight-sum/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
+# Language: python3
+# Date: 2026-02-09
+
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        The result is undefined if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        The result is undefined if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        deq = deque(nestedList)
+        res = 0
+        depth = 1
+
+        while deq:
+            for i in range(len(deq)):
+                cur = deq.popleft()
+
+                if cur.isInteger():
+                    res += depth * cur.getInteger()
+                else:
+                    deq.extend(cur.getList())
+            depth += 1
+        
+        return res
