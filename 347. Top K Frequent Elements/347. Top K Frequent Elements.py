06dcaa3533781387b9e777953cac4c3c19d2f65f
Vasavya Sri <vasavyasritm@gmail.com>
2026-02-08 23:00:54 -0800

    Update solution: 347. Top K Frequent Elements

--- 347. Top K Frequent Elements/347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements/347. Top K Frequent Elements.py
@@ -1,23 +1,24 @@
 #
 # Problem: 347. Top K Frequent Elements
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/top-k-frequent-elements/description/
+# Link: https://leetcode.com/problems/top-k-frequent-elements/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 # Language: python3
-# Date: 2026-01-11
+# Date: 2026-02-09
 
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = [[] for _ in range(len(nums)+1)]
         counter = Counter(nums)
+# k is in the range [1, the number of unique elements in the array]
+        freq = [[] for i in range(len(nums)+1)]
 
         for i, c in counter.items():
             freq[c].append(i)
         res = []
+        
         for i in range(len(freq)-1, -1, -1):
-            if freq[i] and k > 0:
-                for n in freq[i]:
-                    res.append(n)
+            if freq[i]!=[] and k > 0:
+                for j in freq[i]:
+                    res.append(j)
                     k -= 1
         return res
-
