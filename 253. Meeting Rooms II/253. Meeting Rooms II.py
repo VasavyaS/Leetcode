d14ae165de0e69dab516327f9f96ff887ec0b227
Vasavya Sri <vasavyasritm@gmail.com>
2026-02-19 17:03:05 -0800

    Add solution: 253. Meeting Rooms II

---
+++ 253. Meeting Rooms II/253. Meeting Rooms II.py
@@ -0,0 +1,26 @@
+#
+# Problem: 253. Meeting Rooms II
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/meeting-rooms-ii/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
+# Language: python3
+# Date: 2026-02-20
+
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        res = 0 
+        s, e = 0, 0
+        count = 0
+
+        start = sorted(i[0] for i in intervals)
+        end = sorted(i[1] for i in intervals)
+
+        while s < len(start):
+            if start[s] < end[e]:
+                count += 1
+                s += 1
+            else:
+                count -= 1
+                e += 1
+            res = max(res, count)
+        return res
