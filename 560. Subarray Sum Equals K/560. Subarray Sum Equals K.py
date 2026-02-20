0e767eab8c961c9fd6e22f475f51302b3380816e
Vasavya Sri <vasavyasritm@gmail.com>
2026-02-20 01:06:47 -0800

    Update solution: 560. Subarray Sum Equals K

--- 560. Subarray Sum Equals K/560. Subarray Sum Equals K.py
+++ 560. Subarray Sum Equals K/560. Subarray Sum Equals K.py
@@ -1,7 +1,7 @@
 #
 # Problem: 560. Subarray Sum Equals K
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/subarray-sum-equals-k/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
+# Link: https://leetcode.com/problems/subarray-sum-equals-k/description/
 # Language: python3
 # Date: 2026-02-20
 
@@ -9,7 +9,7 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         res = 0
-        prefixsum = {0:1}
+        prefixsum = {0:1} # {prefixsum - k : count}
         cursum = 0
 
         for n in nums:
