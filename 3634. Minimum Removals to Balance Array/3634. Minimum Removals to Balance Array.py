db248775dbe096457012825df3dc5cf2745ccdcc
Vasavya Sri <vasavyasritm@gmail.com>
2026-02-23 19:27:41 -0800

    Update solution: 3634. Minimum Removals to Balance Array

--- 3634. Minimum Removals to Balance Array/3634. Minimum Removals to Balance Array.py
+++ 3634. Minimum Removals to Balance Array/3634. Minimum Removals to Balance Array.py
@@ -1,7 +1,7 @@
 #
 # Problem: 3634. Minimum Removals to Balance Array
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/minimum-removals-to-balance-array/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
+# Link: https://leetcode.com/problems/minimum-removals-to-balance-array/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 # Language: python3
 # Date: 2026-02-24
 
@@ -9,12 +9,13 @@
 class Solution:
     def minRemoval(self, nums: List[int], k: int) -> int:
         nums.sort()
-        r = 0
         n = len(nums)
-        ans = n
-
-        for l in range(n):
-            while r < n and nums[r] <= k * nums[l]:
-                r += 1
-            ans = min(ans,n-(r-l))
-        return ans
+        i = 0
+        j = 0
+        resLen = 0
+        while j < n:
+            while i < j and nums[j] > nums[i] * k: #shrink the window until the condition is not satisfied
+                i += 1
+            resLen = max(resLen, (j - i+ 1)) #max len --> min deletions
+            j += 1
+        return n - resLen
