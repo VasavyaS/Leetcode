579000a552577481a6312e9d9113aa19a1e14a3a
Vasavya Sri <vasavyasritm@gmail.com>
2026-04-17 22:48:23 -0700

    Add solution: 364. Nested List Weight Sum II

---
+++ 364. Nested List Weight Sum II/364. Nested List Weight Sum II.py
@@ -0,0 +1,64 @@
+#
+# Problem: 364. Nested List Weight Sum II
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/nested-list-weight-sum-ii/submissions/1981464828/
+# Language: python3
+# Date: 2026-04-18
+
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        total, cur_total = 0 ,0
+        que = deque(nestedList)
+        while que:
+            for i in range(len(que)):
+                cur = que.popleft()
+                if cur.isInteger():
+                    cur_total += cur.getInteger()
+                else:
+                    que.extend(cur.getList())
+            total += cur_total
+        return total
