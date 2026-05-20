50d7b933411a1d5dc832e832d4531e87b1e75899
Vasavya Sri <vasavyasritm@gmail.com>
2026-05-20 00:50:02 -0700

    Update solution: 1101. The Earliest Moment When Everyone Become Friends

--- 1101. The Earliest Moment When Everyone Become Friends/1101. The Earliest Moment When Everyone Become Friends.py
+++ 1101. The Earliest Moment When Everyone Become Friends/1101. The Earliest Moment When Everyone Become Friends.py
@@ -54,36 +54,3 @@ class UnionFind:
 
         return is_merged
         
-class UnionFind:
-
-    def __init__(self, size):
-        self.group = [group_id for group_id in range(size)]
-        self.rank = [0] * size
-
-    def find(self, person):
-        if self.group[person] != person:
-            self.group[person] = self.find(self.group[person])
-        return self.group[person]
-
-    def union(self, a, b):
-        """
-            return: true if a and b are not connected before
-                otherwise, connect a with b and then return false
-        """
-        group_a = self.find(a)
-        group_b = self.find(b)
-        is_merged = False
-        if group_a == group_b:
-            return is_merged
-
-        is_merged = True
-        # Merge the lower-rank group into the higher-rank group.
-        if self.rank[group_a] > self.rank[group_b]:
-            self.group[group_b] = group_a
-        elif self.rank[group_a] < self.rank[group_b]:
-            self.group[group_a] = group_b
-        else:
-            self.group[group_a] = group_b
-            self.rank[group_b] += 1
-
-        return is_merged
