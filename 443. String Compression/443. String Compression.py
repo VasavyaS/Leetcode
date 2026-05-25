c40cc864ce3a2f04ed440e6746039fda4ec8d77b
Vasavya Sri <vasavyasritm@gmail.com>
2026-05-24 20:35:23 -0700

    Update solution: 443. String Compression

--- 443. String Compression/443. String Compression.py
+++ 443. String Compression/443. String Compression.py
@@ -1,15 +1,15 @@
 #
 # Problem: 443. String Compression
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/string-compression/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
+# Link: https://leetcode.com/problems/string-compression/?envType=company&envId=palo-alto-networks&favoriteSlug=palo-alto-networks-all
 # Language: python3
-# Date: 2026-02-20
+# Date: 2026-05-25
 
 
 class Solution:
     def compress(self, chars: List[str]) -> int:
         i = 0
-        newidx = 0
+        newIdx = 0
 
         while i < len(chars):
             ch = chars[i]
@@ -17,12 +17,13 @@ class Solution:
             while (i+1) < len(chars) and chars[i+1] == ch:
                 count += 1
                 i += 1
-            chars[newidx] = ch
-            newidx += 1
+            chars[newIdx] = ch
+            newIdx += 1
             if count > 1:
                 for c in str(count):
-                    chars[newidx] = c
-                    newidx += 1
+                    chars[newIdx] = c
+                    newIdx += 1
+            
             i += 1
-        return newidx
-# O(n) O(1)
+        
+        return newIdx
